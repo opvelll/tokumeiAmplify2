a924d612fb2966ed510889f1e93a4d567e63683b
opvelll <opvel.hero@gmail.com>
2021-07-10 23:35:31 +0900

    [chore] amplify add api and inport amplify library

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+import Amplify from "aws-amplify";
+import aws_exports from "./aws-exports";
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from "@aws-amplify/ui-components/loader";
+
+Amplify.configure(aws_exports);
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
+
+createApp(App)
+  .use(router)
+  .mount("#app");
